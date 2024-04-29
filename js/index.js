84ea4e3e4f50a98ee330b82e39668fab541193bd
agustinnava <agustinnava0@gmail.com>
2024-04-29 20:41:50 -0300

    primer commit

---
+++ js/index.js
@@ -0,0 +1,193 @@
+let buenosaires = ["Selecciona un instituto", "Universidad Nacional de la Matanza"];
+let cordoba = ["Selecciona un instituto", "Universidad Nacional de Córdoba"];
+let entrerios = ["Selecciona un instituto", "Universidad Nacional de Entre Ríos"];
+let santafe = ["Selecciona un instituto", "Universidad Católica de Santa Fe"];
+
+let institutosbuenosaires = [
+    {
+        "establecimiento": 1,
+        "instituto": "Universidad Nacional de la Matanza",
+        "direccion": "Florencio Varela 3400",
+        "Localidad": "San justo",
+        "Provincia": "Buenos Aires",
+        "Email": "matanza@mt.gov.ar"
+    }
+];
+let institutoscordoba = [
+    {
+        "establecimiento": 1,
+        "instituto": "Universidad Nacional de Córdoba",
+        "direccion": "Av. Haya de la Torre",
+        "Localidad": "Córdoba Capital",
+        "Provincia": "Córdoba",
+        "Email": "cordoba@cd.gov.ar"
+    }
+];
+let institutosentrerios = [
+    {
+        "establecimiento": 1,
+        "instituto": "Universidad Nacional de Entre Ríos",
+        "direccion": "Eva Duarte de Perón 24",
+        "Localidad": "Concepción del Uruguay",
+        "Provincia": "Entre Ríos",
+        "Email": "entrerios@er.gov.ar"
+    }
+];
+let institutossantafe = [
+    {
+        "establecimiento": 1,
+        "instituto": "Universidad Católica de Santa Fe",
+        "direccion": "Cngo. Echagüe 71511",
+        "Localidad": "Ciudad de Santa Fe",
+        "Provincia": "Santa Fe",
+        "Email": "santafe@sf.gov.ar"
+    }
+];
+
+let precios = [
+    {
+        "idioma": 1,
+        "precio": "$1000"
+    },
+    {
+        "idioma": 2,
+        "precio": "$1500"
+    },
+    {
+        "idioma": 3,
+        "precio": "$2000"
+    },
+    {
+        "idioma": 4,
+        "precio": "$2500"
+    },
+    {
+        "idioma": 5,
+        "precio": "$3000"
+    },
+    {
+        "idioma": 6,
+        "precio": "$3500"
+    },
+    {
+        "idioma": 7,
+        "precio": "$4000"
+    },
+    {
+        "idioma": 8,
+        "precio": "$4500"
+    },
+]
+
+$(document).ready(function () {
+    $("#slider").slick({
+        autoplay: true,
+        infinite: true
+    });
+    $("#form").submit(function () {
+        guardarDatos();
+        return validar();
+    });
+    $("#provincia").change(function () {
+        combodesplegable($(this).val());
+    });
+    $("#instituto").change(function () {
+        mostrarinfo($(this).val());
+    });
+    $("#idioma").change(function () {
+        mostrarprecio($(this).val());
+    });
+});
+
+/*muestra los institutos de cada provincia*/
+function combodesplegable(valor) {
+    $("#instituto").empty();
+    if (valor == 1) {
+        //lleno el combo con los array
+        for (i in buenosaires) {
+            $("#instituto").append(`<option value="${i}">${buenosaires[i]}</option>`)
+        }
+    } else if (valor == 2) {
+        for (i in cordoba) {
+            $("#instituto").append(`<option value="${i}">${cordoba[i]}</option>`)
+        }
+    } else if (valor == 3) {
+        for (i in entrerios) {
+            $("#instituto").append(`<option value="${i}">${entrerios[i]}</option>`)
+        }
+    } else if (valor == 4) {
+        for (i in santafe) {
+            $("#instituto").append(`<option value="${i}">${santafe[i]}</option>`)
+        }
+    }
+}
+
+/*muestra la informacion del instituto elegido de la provincia elegida, a traves de un array de objetos*/
+function mostrarinfo(instituto) {
+    const valor = $("#provincia").val();
+
+    if (valor == 1) {
+        const institutoprovincia = institutosbuenosaires;
+        filtrarinstitutos(institutoprovincia, instituto);
+    } else if (valor == 2) {
+        const institutoprovincia = institutoscordoba;
+        filtrarinstitutos(institutoprovincia, instituto);
+    } else if (valor == 3) {
+        const institutoprovincia = institutosentrerios;
+        filtrarinstitutos(institutoprovincia, instituto);
+    } else if (valor == 4) {
+        const institutoprovincia = institutossantafe;
+        filtrarinstitutos(institutoprovincia, instituto);
+    }
+}
+
+function filtrarinstitutos(institutoprovincia, instituto) {
+    let filtrado = institutoprovincia.filter(institutoprovincia => institutoprovincia.establecimiento == instituto);
+    $("#informacion").empty();
+    filtrado.forEach(institutoprovincia => {
+        $("#informacion").append(`
+            <div>
+                <h5>Establecimiento:</h5>
+                <p>${institutoprovincia.instituto}</p>
+                <h5>Dirección:</h5>
+                <p>${institutoprovincia.direccion}</p>
+                <h5>Localidad:</h5>
+                <p>${institutoprovincia.Localidad}</p>
+                <h5>Provincia:</h5>
+                <p>${institutoprovincia.Provincia}</p>
+                <h5>Email:</h5>
+                <p>${institutoprovincia.Email}</p>
+            </div>
+        `);
+    });
+}
+
+function mostrarprecio(valor) {
+    let filtrado = precios.filter(precios => precios.idioma == valor);
+    $("#precio").empty();
+    filtrado.forEach(precios => {
+        $("#precio").append(`<p>${precios.precio}</p>`);
+    });
+}
+function validar() {
+    if ($("#provincia, #instituto, #idioma, #hora").is(`:selected`)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function guardarDatos() {
+    let provincia = $("#provincia").val();
+    let instituto = $("#instituto").val();
+    let idioma = $("#idioma").val();
+    let hora = $("#hora").val();
+    let precio = $("#precio").text();
+    
+    if ($("#provincia, #instituto, #idioma, #hora").is(`:selected`)) {
+        localStorage.setItem("provincia", provincia);
+        localStorage.setItem("instituto", instituto);
+        localStorage.setItem("idioma", idioma);
+        localStorage.setItem("hora", hora);
+        localStorage.setItem("precio", precio);
+    }
+}
---
+++ js/inscripcion.js
@@ -0,0 +1,117 @@
+$(document).ready(function () {
+    $("#form").submit(function () {
+        return validar();
+    });
+    $("#nombre, #apellido, #tel, #provincia, #ciudad").submit(function () {
+        validar();
+    });
+    $("#email").submit(function () {
+        validar("#email");
+    });
+    let provincia = localStorage.getItem("provincia");
+    let instituto = localStorage.getItem("instituto");
+    let idioma = localStorage.getItem("idioma");
+    let hora = localStorage.getItem("hora");
+    let precio = localStorage.getItem("precio");
+    $("#mensaje1").append(`Inscripción al curso de ${idioma} en la provincia de ${provincia} y sede ${instituto} en el horario ${hora}.`);
+    $("#mensaje2").append(`El precio es de ${precio}.-`);
+});
+
+function validar() {
+    var regexemail = /^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[0-9a-zA-Z]+$/;
+    var mensajenombre = "<p>El campo nombre es obligatorio</p>";
+    var mensajeapellido = "<p>El campo apellido es obligatorio</p>";
+    var mensajeemail = "<p>El email ingresado no es valido</p>";
+    var mensajetel = "<p>El numero de telefono es obligatorio</p>";
+    var mensajeprovincia = "<p>El campo provincia es obligatorio</p>";
+    var mensajeciudad = "<p>El campo ciudad es obligatorio</p>";
+    var error = 0;
+
+    reset();
+
+    if ($("#nombre").val() == "") {
+        error++;
+        $("#mensajenombre").append(mensajenombre)
+        $("#nombre").addClass('error');
+        $("#nombre").keyup(function () { /*valida en tiempo real luego que se muestra el error*/
+            validar();
+        });
+    }
+
+    /*valida que apellido no este vacio*/
+    if ($("#apellido").val() == "") {
+        error++;
+        $("#mensajeapellido").append(mensajeapellido)
+        $("#apellido").addClass('error');
+        $("#apellido").keyup(function () {
+            validar();
+        });
+    }
+
+    /*valida el email*/
+    if (!$("#email").val().match(regexemail)) {
+        error++;
+        $("#mensajeemail").append(mensajeemail)
+        $("#email").addClass('error');
+        $("#email").keyup(function () {
+            validar("#email");
+        });
+    }
+
+    /*valida el telefono*/
+    if ($("#tel").val() == "") {
+        error++;
+        $("#mensajetel").append(mensajetel)
+        $("#tel").addClass('error');
+        $("#tel").keyup(function () {
+            validar();
+        });
+    }
+
+    if ($("#provincia").val() == "") {
+        error++;
+        $("#mensajeprovincia").append(mensajeprovincia)
+        $("#provincia").addClass('error');
+        $("#provincia").keyup(function () {
+            validar();
+        });
+    }
+
+    if ($("#ciudad").val() == "") {
+        error++;
+        $("#mensajeciudad").append(mensajeciudad)
+        $("#ciudad").addClass('error');
+        $("#ciudad").keyup(function () {
+            validar();
+        });
+    }
+
+    if (error > 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function reset() {
+    $("#nombre").removeClass('error');
+    $("#apellido").removeClass('error');
+    $("#email").removeClass('error');
+    $("#tel").removeClass('error');
+    $("#provincia").removeClass('error');
+    $("#ciudad").removeClass('error');
+    $("#mensajenombre").empty();
+    $("#mensajeapellido").empty();
+    $("#mensajeemail").empty();
+    $("#mensajetel").empty();
+    $("#mensajeprovincia").empty();
+    $("#mensajeciudad").empty();
+}
+
+let provincia = localStorage.getItem("provincia");
+let instituto = localStorage.getItem("instituto");
+let idioma = localStorage.getItem("idioma");
+let hora = localStorage.getItem("hora");
+let precio = localStorage.getItem("precio");
+$("#mensaje1").append(`<p>Inscripción al curso de ${idioma} en la provincia de ${provincia} y sede ${instituto} en el horario ${hora}.</p>`);
+$("#mensaje2").append(`El precio es de ${precio}.-`);
